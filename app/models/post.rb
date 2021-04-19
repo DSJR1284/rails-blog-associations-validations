148228145bbe42dec2d2bdceeb48bee8460d0323
Doug Jones <DSJR1284@GMAIL.COM>
2021-04-18 22:08:33 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,8 @@
 class Post < ActiveRecord::Base
-  
+  validates :name, presence: :true
+  validates_presence_of :content 
+
+  belongs_to :user 
+  has_many :post_tags 
+  has_many :tags, through: :post_tags 
 end
